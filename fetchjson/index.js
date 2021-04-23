224df889da43dd05ba9ef32ec34af497d455ad41
lai <doublewlai@gmail.com>
2021-04-23 21:58:22 +0300

    introduce ts to fix the variable name issue

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,10 +3,10 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://reqres.in/api/users/2';
 axios_1["default"].get(url).then(function (res) {
-  const user = res.data;
-  const ID = user.data.id;
-  const email = user.data.email;
-  const name = user.data.first_name;
+  const user = res.data.data;
+  const ID = user.id;
+  const email = user.email;
+  const name = user.first_name;
 
   console.log(`
     my id is: ${ID}
--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,21 @@ import axios from 'axios';
 
 const url = 'https://reqres.in/api/users/2';
 
+interface User {
+  id: number;
+  email: string;
+  first_name: string;
+}
+
 axios.get(url).then((res) => {
-  console.log(res.data);
+  const user = res.data.data as User;
+  const id = user.id;
+  const email = user.email;
+  const name = user.first_name;
+
+  console.log(`
+    my id is: ${id}
+    my email is: ${email}
+    my name is: ${name}
+  `);
 });
