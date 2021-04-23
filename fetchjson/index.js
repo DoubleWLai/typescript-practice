b33394413ff570be9f98afb925b1359cf6bb9e1f
lai <doublewlai@gmail.com>
2021-04-23 21:20:57 +0300

    print string with response data

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,14 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://reqres.in/api/users/2';
 axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
+  const user = res.data;
+  const ID = user.data.id;
+  const email = user.data.email;
+  const name = user.data.first_name;
+
+  console.log(`
+    my id is: ${ID}
+    my email is: ${email}
+    my name is: ${name}
+  `);
 });
