e1443891f86bfc214b9648afaec0ff65c9c8705b
lai <doublewlai@gmail.com>
2021-04-25 12:06:22 +0300

    try ts class function

--- features/annotations/function.ts
+++ features/annotations/function.ts
@@ -1,3 +1,17 @@
 const add = (a: number, b: number) => {
   return a + b;
 };
+
+
+const obj = {
+  name: 'lai',
+  age: 22,
+};
+
+const n = obj.age;
+
+class Vehicle {
+  drive(): void {
+    console.log('chahchahcahca')
+  }
+}
