1dfd68d2c8d8e8e3a7bc78388e5be08ae5bd12b5
lai <doublewlai@gmail.com>
2021-05-01 08:44:08 +0300

    move specific data process logic into separate function

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -18,16 +18,21 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult, // 'as' used to tell TS we know what the type should be at here.
-          row[6],
-        ]
-      });
+      .map(this.mapRow);
+  }
+
+
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult, // 'as' used to tell TS we know what the type should be at here.
+      row[6],
+    ];
   }
 }
+
+
