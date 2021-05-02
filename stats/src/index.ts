d9f4870581d2d4f1e2dcdcd838596025356c5e3c
lai <doublewlai@gmail.com>
2021-05-02 10:39:36 +0300

    create reporter class

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,5 +1,4 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResults';
 import { CsvFileReader } from './CsvFileReader';
 
 const csvFileReader = new CsvFileReader('football.csv');
@@ -7,14 +6,4 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
 
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(manUnitedWins);
