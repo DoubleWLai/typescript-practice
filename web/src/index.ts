2828a7ec9fe8e82d513d99de05b2fd1b465c0589
lai <doublewlai@gmail.com>
2021-05-04 20:46:30 +0300

    add optional properties in interface

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,8 @@
-console.log('ssssss');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.set({ name: 'newName' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
