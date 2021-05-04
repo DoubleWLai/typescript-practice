78c67cfcd2af2f85a1cd92cc8582d246d5d8c481
lai <doublewlai@gmail.com>
2021-05-04 22:24:56 +0300

    implement function on

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'newName' });
+user.on('sss', () => {
+  console.log(111);
+});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('sss', () => {
+  console.log(111);
+});
+
+user.on('ssaaas', () => {
+  console.log(111);
+});
+
+console.log(user);
