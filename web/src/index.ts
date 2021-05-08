5423501196d960716dc0664eb70ed4926f5f2d89
lai <doublewlai@gmail.com>
2021-05-08 18:18:43 +0300

    add coordinated set function

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,3 +3,9 @@ import { User } from './models/User';
 const user = new User({ name: 'newnewnew', age: 12312 });
 
 console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.set({ name: 'New name' });
